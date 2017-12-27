ceff6bf728b6ee45e5932bbf10a80dfcd949d83c
JoshBurke <joshDgreat37@gmail.com>
2017-12-27 01:22:41 -0600

    Update 1.1
    See readme

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
 
 # Conway's Game of Life
-This is my work-in-progess implementation of Conway's Game of Life. I've always been fascinated by the concept of emergence, and Conway's Game is a nice demonstration of how simple processes can work together to become more complex.
-This project is also serving as my first solo web project.
-
-Going forward with this project I'd like to optimize the simulation. It runs pretty slow at this point. Another task would be to automate the process of finding repeating patterns - running the simulation for a few thousand iterations and then locating patterns with period > 2. I'd also like to add options for seed density, seed serializing/deserializing, replaying seeds, starting new seeds *without* refreshing, and maybe some time controls once the simulation is fast enough. Once I learn more about common repeaters I think it would be cool to be able to load examples of them as seeds, so that someone new to the game could learn about them as well.
-
 ## Functionality:
 As of now, each time the page is loaded it has a random seed. Click to play/pause.
 
 You can run the simulation by clicking [here](http://htmlpreview.github.com/?https://github.com/JoshBurke/Conway/blob/master/docs/index.html).
 
 -Josh Burke
+
+## Update Notes:
+
+### December 27, 2017
+
+This update:
+* New settings menu next to simulation display
+* Variable size, density, and fps settings
+* Some slight optimization to simulation
+* Frame delay display
+* Fixed bug with bottom row and rightmost column, where they weren't being rendered correctly
+* New CSS stylesheet
+
+### December 25, 2017
+
+This is my work-in-progess implementation of Conway's Game of Life. I've always been fascinated by the concept of emergence, and Conway's Game is a nice demonstration of how simple processes can work together to become more complex.
+This project is also serving as my first solo web project.
+
+Going forward with this project I'd like to optimize the simulation. It runs pretty slow at this point. Another task would be to automate the process of finding repeating patterns - running the simulation for a few thousand iterations and then locating patterns with period > 2. I'd also like to add options for seed density, seed serializing/deserializing, replaying seeds, starting new seeds *without* refreshing, and maybe some time controls once the simulation is fast enough. Once I learn more about common repeaters I think it would be cool to be able to load examples of them as seeds, so that someone new to the game could learn about them as well.
--- docs/js/conway.js
+++ docs/js/conway.js
@@ -5,59 +5,60 @@ var stage = acgraph.create('container');
 // get bounds of stage
 var bounds = stage.getBounds();
 
-// calculate chip width/height
-var XDIMENSION = 100;
-var YDIMENSION = 100;
+// parameters
+var XDIMENSION = 50;
+var YDIMENSION = 50;
 var START_DENSITY = 0.07;
 var CELL_WIDTH = bounds.width / XDIMENSION;
 var CELL_HEIGHT = bounds.height / YDIMENSION;
+var MAX_HZ = 60;
 
 var grid;
 var running = false;
 
+var fpstext;
+var framedelay = 0;
+
 function main(){
-  stage.suspend();
   generateGrid();
-  stage.resume();
   draw();
 }
 
-function run(){
-    oneStep();
-
-}
-
 function generateGrid(){
+  stage.suspend();
+  acgraph.events.listen(stage, "click", function(){ running = !running;});
   grid = new Array(XDIMENSION);
-  for(i = 0; i < XDIMENSION; i++){
+  for(var i = 0; i < XDIMENSION; i++){
     grid[i] = new Array(YDIMENSION);
-    for(j = 0; j < YDIMENSION; j++){
+    for(var j = 0; j < YDIMENSION; j++){
       v = Math.random() < START_DENSITY ? true : false;
       grid[i][j] = new Cell(j, i, v);
-      console.log("Created cell " + j + ", " + i);
     }
   }
+  stage.resume();
 }
 
 function oneStep(){
+  var time = (new Date()).getTime();
   stage.suspend();
-  for(i = 0; i < XDIMENSION; i++){
-    for(j = 0; j < YDIMENSION; j++){
-      console.log("Stepping for cell " + j + ", " + i);
+  for(var i = 0; i < XDIMENSION; i++){
+    for(var j = 0; j < YDIMENSION; j++){
       grid[i][j].getNextValue();
     }
   }
 
-  for(i = 0; i < XDIMENSION; i++){
-    for(j = 0; j < YDIMENSION; j++){
-      grid[i][j].update();
+  for(var i = 0; i < XDIMENSION; i++){
+    for(var j = 0; j < YDIMENSION; j++){
+      if(grid[i][j].val != grid[i][j].nextVal){
+        grid[i][j].update();
+      }
     }
   }
   stage.resume();
+  var finishtime = (new Date()).getTime();
+  framedelay = (finishtime - time);
+  fpstext.innerHTML = "Frame time: " + framedelay + "ms + " + Math.max(Math.round(1000 / MAX_HZ - framedelay), 0) + "ms";
 }
-
-acgraph.events.listen(stage, "click", function(){ running = !running;});
-//var button = step.rect(step.getBounds.width/2, step.getBounds.height/2, 40, 5).fill('red');
 
 function Cell(x, y, val){
   this.x = x;
@@ -71,6 +72,11 @@ function Cell(x, y, val){
   }
 }
 
+function DestroyGrid(){
+  stage.dispose();
+  stage = acgraph.create('container');
+}
+
 Cell.prototype.setVal = function(val){
   this.val = val;
 }
@@ -81,8 +87,12 @@ Cell.prototype.getVal = function(){
 
 Cell.prototype.getNextValue = function(){
   var count = 0;
-  for(a = Math.max(0, 1 - this.y); a < Math.min(3, XDIMENSION - this.y); a++){
-    for(b = Math.max(0, 1 - this.x); b < Math.min(3, YDIMENSION - this.x); b++){
+  var amin = Math.max(0, 1 - this.y);
+  var amax = Math.min(3, XDIMENSION - this.y + 1);
+  var bmin = Math.max(0, 1 - this.x);
+  var bmax = Math.min(3, YDIMENSION - this.x + 1);
+  for(var a = amin; a < amax; a++){
+    for(var b = bmin; b < bmax; b++){
       if(a == 1 && b == 1) continue;
       if(grid[this.y + a - 1][this.x + b - 1].getVal()){
         count++;
@@ -107,19 +117,94 @@ Cell.prototype.update = function(){
   }
 }
 
-window.requestAnimationFrame = window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function (callback) {
-            setTimeout(callback, 1000 / 60);
+window.requestAnimationFrame = function (callback) {
+            setTimeout(callback, Math.max(1000 / MAX_HZ - framedelay, 0));
         };
 
 function draw(){
   window.requestAnimationFrame(draw);
-  if(running)
+  if(running){
     oneStep();
+  }
 }
 
 main();
+
+/*-------------------------------------------------------*/
+
+var createform = document.getElementById("controls");
+
+var heading = document.createElement('h2');
+heading.innerHTML = "Settings ";
+createform.appendChild(heading);
+
+var line = document.createElement('hr');
+createform.appendChild(line);
+
+var linebreak = document.createElement('br');
+createform.appendChild(linebreak);
+
+var sizelabel = document.createElement('label');
+sizelabel.innerHTML = "Grid size (NxN): "; 
+createform.appendChild(sizelabel);
+
+var sizeelement = document.createElement('input'); 
+sizeelement.setAttribute("type", "number");
+sizeelement.setAttribute("value", XDIMENSION);
+sizeelement.setAttribute("name", "dsize");
+createform.appendChild(sizeelement);
+
+var linebreak = document.createElement('br');
+createform.appendChild(linebreak);
+
+var densitylabel = document.createElement('label');
+densitylabel.innerHTML = "Starting Density: ";
+createform.appendChild(densitylabel);
+
+var densityelement = document.createElement('input');
+densityelement.setAttribute("type", "number");
+densityelement.setAttribute("value", START_DENSITY);
+densityelement.setAttribute("name", "ddensity");
+createform.appendChild(densityelement);
+
+var densitybreak = document.createElement('br');
+createform.appendChild(densitybreak);
+
+var timelabel = document.createElement('label');
+timelabel.innerHTML = "Max refresh rate (hz): ";
+createform.appendChild(timelabel);
+
+var timeelement = document.createElement('input');
+timeelement.setAttribute("type", "number");
+timeelement.setAttribute("value", MAX_HZ);
+timeelement.setAttribute("name","dtime");
+createform.appendChild(timeelement);
+
+var timebreak = document.createElement('br');
+createform.appendChild(timebreak);
+
+var submitelement = document.createElement('input');
+submitelement.setAttribute("type", "button");
+submitelement.setAttribute("name", "dsubmit");
+submitelement.setAttribute("value", "Redraw grid");
+submitelement.addEventListener("click", function(){
+    XDIMENSION = sizeelement.value;
+    YDIMENSION = sizeelement.value;
+    START_DENSITY = densityelement.value;
+    running = false;
+    CELL_WIDTH = bounds.width / XDIMENSION;
+    CELL_HEIGHT = bounds.height / YDIMENSION;
+    MAX_HZ = timeelement.value;
+    DestroyGrid();
+    generateGrid();
+});
+createform.appendChild(submitelement);
+
+var xbreak = document.createElement('br');
+var xxbreak = document.createElement('br');
+createform.appendChild(xbreak);
+createform.appendChild(xxbreak);
+
+fpstext = document.createElement('label');
+fpstext.innerHTML = "Frame delay: N/a";
+createform.appendChild(fpstext);
